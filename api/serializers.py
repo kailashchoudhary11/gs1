4a84f3b9c74116f92b826b5ed44145fb5fce0628
cobra <kainewhere@gmail.com>
2022-05-17 18:01:15 +0530

    Added a Student Serialzer for Student Model

--- api/serializers.py
+++ api/serializers.py
@@ -3,5 +3,9 @@ from time import clock_settime
 from rest_framework import serializers
 from .models import Student
 
-class StudentSerializer(serializers.ModelSerializer):
-    pass
+# class StudentSerializer(serializers.ModelSerializer):
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length = 100)
+    prn = serializers.IntegerField()
+    city = serializers.CharField(max_length = 100)
+    
