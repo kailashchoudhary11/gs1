12485de6bd836393c7d1db6285cf4caa41b15dfc
cobra <kainewhere@gmail.com>
2022-05-17 18:19:46 +0530

    Added str method to model

--- api/models.py
+++ api/models.py
@@ -6,3 +6,6 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     prn = models.IntegerField()
     city = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
