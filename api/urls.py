ec72a5af291289796a74d9a617512479b2888601
kai <kailashjat604@gmail.com>
2022-05-18 07:11:44 +0530

    Added urls for detail and list views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import student_detail
+from .views import student_detail, student_list
 app_name = "api"
 
 urlpatterns = [
-    path("", student_detail, name="student_detail"),
+    path("<int:pk>/", student_detail, name="student_detail"),
+    path("all/", student_list, name="student_list"),
 ]
